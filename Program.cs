07d8a1e27f237de3b367e8f9d4e08830c4b5abc5
Viktor Karlsson <viktor.karlsson2000@gmail.com>
2024-04-24 21:58:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using XmlConverterConsole;
+
+const bool runner = true;
+Console.WriteLine(" Welcome to this simple Xml-Converter! \n Enter 'start' to begin or exit to quit");
+
+while (runner)
+{
+    var input = Console.ReadLine();
+
+
+    if (input is null or "")
+    {
+        Console.WriteLine("Enter 'start' to begin or exit to quit");
+        continue;
+    }
+
+    if (input.ToLower().Equals("exit"))
+    {
+        Console.WriteLine(" Exiting program");
+        Environment.Exit(0);
+    }
+
+
+    if (input.ToLower().Equals("start"))
+    {
+        try
+        {
+            Console.Write(" File to read: ");
+            var file = Console.ReadLine();
+            Console.Write("\n Xml-name and path: ");
+            var savePath = Console.ReadLine() ?? string.Empty;
+
+
+            var fileContent = Services.ReadFile(file ?? throw new Exception(" No file found"));
+
+            Services.CreatePeopleFromFile(fileContent, savePath);
+
+            Console.WriteLine(" Xml has been generated from file");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            continue;
+        }
+    }
+}
---
+++ Services.cs
@@ -0,0 +1,141 @@
+﻿using System.Xml.Linq;
+
+
+namespace XmlConverterConsole
+{
+    internal class Services
+    {
+        public static string ReadFile(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public static void CreatePeopleFromFile(string data, string savePath = @"C:\MyXmlFile.xml")
+        {
+            try
+            {
+                var dataRows = data.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                var document = new XDocument(new XElement("people"));
+                var previousNodeType = "";
+
+                foreach (var parts in dataRows.Select(row => row.Split('|')))
+                {
+                    previousNodeType = parts[0] switch
+                    {
+                        "P" => "person",
+                        "F" => "family",
+                        _ => previousNodeType
+                    };
+                    switch (parts[0])
+                    {
+                        case "P":
+                            XmlAddPerson(parts, document);
+                            break;
+
+                        case "T":
+                            XmlAddPhone(parts, previousNodeType, document);
+                            break;
+
+                        case "A":
+                            XmlAddAddress(parts, previousNodeType, document);
+                            break;
+
+                        case "F":
+                            XmlAddFamily(parts, previousNodeType, document);
+                            break;
+                    }
+                }
+
+                document.Save(savePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+
+        private static void XmlAddPerson(string[] data, XContainer document)
+        {
+            var element = document.Element("people") ?? throw new Exception("Element People does not exist");
+            element.Add(
+                new XElement("person",
+                    new XElement("firstname", data[1]),
+                    new XElement("lastname", data[2])));
+        }
+
+        private static void XmlAddPhone(string[] data, string previousNodeType, XContainer document)
+        {
+            var lastPerson = document.Descendants(previousNodeType).LastOrDefault() ??
+                             throw new Exception($"Node {previousNodeType} does not exist");
+
+            foreach (var (part, index) in data.Select((value, index) => (value, index)))
+            {
+                if (index == 0) continue;
+
+                switch (index)
+                {
+                    case 1:
+                        lastPerson.Add(new XElement("phone",
+                            new XElement("mobile", part)));
+                        break;
+                    case 2:
+                        lastPerson = document.Descendants("phone").LastOrDefault() ??
+                                     throw new Exception($"Node {previousNodeType} does not exist");
+                        lastPerson.Add(new XElement("landline", part));
+                        break;
+                }
+            }
+        }
+
+        private static void XmlAddAddress(string[] data, string previousNodeType, XContainer document)
+        {
+            var lastPerson = document.Descendants(previousNodeType).LastOrDefault() ??
+                             throw new Exception($"Node {previousNodeType} does not exist");
+            foreach (var (part, index) in data.Select((value, index) => (value, index)))
+            {
+                if (index == 0) continue;
+
+                switch (index)
+                {
+                    case 1:
+                        lastPerson.Add(new XElement("address",
+                            new XElement("street", part)));
+                        break;
+                    case 2:
+                        lastPerson = document.Descendants("address").LastOrDefault() ??
+                                     throw new Exception($"Node {previousNodeType} does not exist");
+                        lastPerson.Add(new XElement("city", part));
+                        break;
+                    case 3:
+                        lastPerson.Add(new XElement("zipcode", part));
+                        break;
+                }
+            }
+        }
+
+        private static void XmlAddFamily(string[] data, string previousNodeType, XContainer document)
+        {
+            var lastPerson = document.Descendants("person").LastOrDefault() ??
+                             throw new Exception($"Node {previousNodeType} does not exist");
+            foreach (var (part, index) in data.Select((value, index) => (value, index)))
+            {
+                if (index == 0) continue;
+
+                switch (index)
+                {
+                    case 1:
+                        lastPerson.Add(new XElement("family",
+                            new XElement("name", part)));
+                        break;
+                    case 2:
+                        lastPerson = document.Descendants("family").LastOrDefault() ??
+                                     throw new Exception($"Node {previousNodeType} does not exist");
+                        lastPerson.Add(new XElement("born", part));
+                        break;
+                }
+            }
+        }
+    }
+}
